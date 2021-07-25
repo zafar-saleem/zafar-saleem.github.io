f29414cce1419862dfcf165a3b064ed1cd079958
Zafar Saleem <zafarsaleem3@gmail.com>
2021-07-25 17:14:32 +0500

    Added CV, fixed mobile view

--- src/components/description.js
+++ src/components/description.js
@@ -1,7 +1,9 @@
 import Skills from './skills';
+import cv from '../assets/cv/CV.pdf';
 import styled from 'styled-components';
 
 const Desc = styled.div`
+  position: relative;
   flex-basis: 66.6%;
   padding: 1em;
   font-family: 'Orbitron', sans-serif;
@@ -9,20 +11,37 @@ const Desc = styled.div`
   letter-spacing: 1px;
   @media screen and (max-width: 500px) {
   	font-size: 14px;
+    flex-basis: 80%;
   }
 `;
 
 const Name = styled.h1`
   display: inline-block;
   margin: 0;
+  @media screen and (max-width: 500px) {
+    font-size: 22px;
+  }
 `;
 
 const Link = styled.a`
+  position:absolute;
   color: #999999;
   text-decoration: none;
   font-size: 12px;
   margin-right: 5px;
   box-sizing: border-box;
+  &.github {
+    left: 78px;
+  }
+  &.linkedin {
+    left: 132px;
+  }
+  &.instagram {
+    left: 200px;
+  }
+  &.cv {
+    left: 282px;
+  }
   &:hover {
     font-size: 13px;
     text-shadow: 0px 0px 10px #12a0a0;
@@ -53,10 +72,11 @@ const Description = () => {
       <Name>Zafar Saleem</Name><Period>_</Period>
       <Skills />
       <p>
-        <Link href='https://zafarsaleem.medium.com'>Medium</Link>
-        <Link href='https://github.com/zafar-saleem'>Github</Link>
-        <Link href='https://www.linkedin.com/in/zeesaleem/'>Linkedin</Link>
-        <Link href='https://www.instagram.com/xafarfotos/'>Instagram</Link>
+        <Link className='medium' href='https://zafarsaleem.medium.com'>Medium</Link>
+        <Link className='github' href='https://github.com/zafar-saleem'>Github</Link>
+        <Link className='linkedin' href='https://www.linkedin.com/in/zeesaleem/'>Linkedin</Link>
+        <Link className='instagram' href='https://www.instagram.com/xafarfotos/'>Instagram</Link>
+        <Link className='cv' href={`${cv}`}>CV</Link>
       </p>
     </Desc>
   )
