9bf8625ed7eb196c21a2670436f3320b35cc6edb
Zafar Saleem <zafarsaleem3@gmail.com>
2021-08-20 10:45:29 +0500

    Updates regarding SEO metadata

--- src/components/dp/dp.js
+++ src/components/dp/dp.js
@@ -65,7 +65,7 @@ const Dp = () => (
 	<DpContainer>
 	  <ArcOuter />
 	  <ArcInner />
-	  <DpImage src={`${avatar}`} alt='JavaScript | HTML | CSS | React | Redux' />
+	  <DpImage src={`${avatar}`} alt='Zafar Saleem | JavaScript | HTML | CSS | React | Redux | Node | Express' />
 	</DpContainer>
 );
 
