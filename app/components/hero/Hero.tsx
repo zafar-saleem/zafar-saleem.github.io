b898dc28c1ab9dfa44d0a3480014019418143bab
Zafar Saleem <zafarsaleem3@gmail.com>
2025-03-01 07:44:34 +0000

    feat: initial commit

---
+++ app/components/hero/Hero.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import { contents } from "./contents";
+
+export const Hero = () => {
+  return (
+    <section className="py-32">
+      <div className="container text-center">
+        <div className="mx-auto flex max-w-screen-lg flex-col gap-6">
+          <h1 className="text-3xl font-extrabold lg:text-6xl">{contents.heading}</h1>
+          <p className="text-balance text-muted-foreground lg:text-lg">
+            {contents.description}
+          </p>
+        </div>
+        <Button asChild size="lg" className="mt-10">
+          <a href={contents.button.url}>{contents.button.text}</a>
+        </Button>
+      </div>
+    </section>
+  )
+}
---
+++ app/components/hero/contents.tsx
@@ -0,0 +1,13 @@
+export const contents = {
+  heading: "Hi, I'M Zafar, Freelance Developer",
+  description: `
+      If your project involves online store, I CAN HELP, here is how:
+      Don't have online store? I will build it for you. 
+      Have out dated website? I will modernize it for you.
+      Don't have e-commerce website? I will built it for you.
+    `,
+  button: {
+    text: "Contact Me",
+    url: "mailto:zsaleem@protonmail.com?subject=Contact from Portfolio Website - ",
+  },
+}
