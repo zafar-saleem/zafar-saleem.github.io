1086790dc473c6e62c6a374cc7138ad934744d77
Zafar Saleem <zafarsaleem3@gmail.com>
2025-03-03 17:16:52 +0000

    fix: minor update

--- app/components/call-to-action/contents.tsx
+++ app/components/call-to-action/contents.tsx
@@ -1,5 +1,5 @@
 export const contents = {
-  heading: "Do Website Development?",
+  heading: "Need Website Development?",
   description: "Click below button to schedule a call with me.",
   buttons: {
     secondary: {
