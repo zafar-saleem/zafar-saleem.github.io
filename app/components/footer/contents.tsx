21572f700bee51fbcd2f1fba9e86a3bec8ce52f2
Zafar Saleem <zafarsaleem3@gmail.com>
2025-03-03 11:15:36 +0000

    feat: updated footer

---
+++ app/components/footer/contents.tsx
@@ -0,0 +1,33 @@
+export const contents = {
+  logo: {
+    title: "Zafar Saleem",
+    url: "https://zafar-saleem.github.io",
+  },
+  tagline: "Freelance Frontend Engineer.",
+  menuItems: [
+    {
+      links: [
+        { text: "About", url: "#about" },
+        { text: "Expertise", url: "#expertise" },
+        { text: "Portfolio", url: "#portfolio" },
+        { text: "Testimonials", url: "#testimonials" },
+      ],
+    },
+    {
+      links: [
+        { text: "Blog", url: "https://medium.com/@zafarsaleem" },
+        { text: "Schedule Meeting", url: "https://calendly.com/zsaleem/meeting" },
+        { text: "Contact me", url: "mailto:zafarsaleem3@gmail.com?subject=I would like to hire you for [enter project name/position]" },
+      ],
+    },
+    {
+      title: "Social",
+      links: [
+        { text: "X (Formerly Twitter)", url: "https://x.com/zafartalks" },
+        { text: "Instagram", url: "https://instagram/dakalfotografy" },
+        { text: "LinkedIn", url: "https://linkedin.com/in/zeesaleem" },
+      ],
+    },
+  ],
+  copyright: "© 2025 Copyright. All rights reserved.",
+}
---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import { contents } from "./contents";
+
+const Footer = () => {
+  return (
+    <section className="py-32 mt-10">
+      <div className="container">
+        <footer>
+          <div className="grid grid-cols-2 gap-8 lg:grid-cols-6">
+            <div className="col-span-2 mb-8 lg:mb-0">
+              <div className="flex items-center gap-2 lg:justify-start">
+                <Link href="/">
+                  <p className="text-xl font-semibold">{contents.logo.title}</p>
+                </Link>
+              </div>
+              <p className="mt-4 font-bold">{contents.tagline}</p>
+            </div>
+            {contents.menuItems.map((section, sectionIdx) => (
+              <div key={sectionIdx}>
+                <h3 className="mb-4 font-bold">{section.title}</h3>
+                <ul className="space-y-4 text-muted-foreground">
+                  {section.links.map((link, linkIdx) => (
+                    <li
+                      key={linkIdx}
+                      className="font-medium hover:text-primary"
+                    >
+                      <Link href={link.url}>{link.text}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+          <div className="mt-24 flex flex-col justify-between gap-4 border-t pt-8 text-sm font-medium text-muted-foreground md:flex-row md:items-center">
+            <p>{contents.copyright}</p>
+          </div>
+        </footer>
+      </div>
+    </section>
+  );
+};
+
+export { Footer };
