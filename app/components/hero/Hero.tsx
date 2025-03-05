1532df53ac6b97c142020850d87cf4f2bd84dca1
Zafar Saleem <zafarsaleem3@gmail.com>
2025-03-05 15:31:09 +0000

    feat: hero desc section added

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -8,10 +8,12 @@ export const Hero = ({ id }: { id: string }) => {
     <section id={id} className="py-32">
       <div className="container text-center">
         <div className="mx-auto flex max-w-screen-lg flex-col gap-6">
-          <h1 className="text-3xl font-extrabold lg:text-6xl">{contents.heading}</h1>
-          <p className="text-balance text-muted-foreground lg:text-lg">
-            {contents.description}
-          </p>
+          <h1 className="text-3xl font-extrabold lg:text-6xl mb-5">{contents.heading}</h1>
+            {
+              contents.descs.map((content, index) => (
+                <p key={index} className="text-balance text-muted-foreground lg:text-lg">{content}</p>
+              ))
+            }
         </div>
         <Button asChild size="lg" className="mt-10">
           <a href={contents.button.url}>{contents.button.text}</a>
--- app/components/hero/contents.tsx
+++ app/components/hero/contents.tsx
@@ -1,11 +1,11 @@
 export const contents = {
   heading: "Hi, I'M Zafar, Freelance Developer",
-  description: `
-      If your project involves online store, I CAN HELP, here is how:
-      Don't have online store? I will build it for you. 
-      Have out dated website? I will modernize it for you.
-      Don't have e-commerce website? I will built it for you.
-    `,
+  descs: [
+    "If your project involves online store, I CAN HELP, here is how:",
+    "Don't have online store? I will build it for you.",
+    "Have out dated website? I will modernize it for you.",
+    "Don't have e-commerce website? I will built it for you."
+  ],
   button: {
     text: "Contact Me",
     url: "mailto:zafarsaleem3@gmail.com?subject=I would like to hire you for [enter project name/position]",
