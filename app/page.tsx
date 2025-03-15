4150a631aade0dafc5b804114af7edbbf542900a
Zafar Saleem <zafarsaleem3@gmail.com>
2025-03-15 09:17:48 +0000

    fix: fixed wrapper padding on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,10 @@ import { Cta } from "./components/call-to-action";
 
 export default function Home() {
   return (
-    <div className="m-auto max-w-7xl">
+    <div className="m-auto max-w-7xl p-3 lg:p-0">
       <Navbar />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start p-3 lg:p-0">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <Hero id="about" />
           <Expertise id="expertise" />
           <Services id="services" />
