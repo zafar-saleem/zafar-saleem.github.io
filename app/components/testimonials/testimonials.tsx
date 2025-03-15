8f475f91a4d024bc1922aa6af42c6273032eec04
Zafar Saleem <zafarsaleem3@gmail.com>
2025-03-15 09:57:37 +0000

    feat: switched testimonials to carousal

--- app/components/testimonials/testimonials.tsx
+++ app/components/testimonials/testimonials.tsx
@@ -1,37 +1,56 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { contents } from "./contents";
+import * as React from "react"
 
-const Testimonial = ({ id }: { id: string }) => {
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import { contents } from "./contents"
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
+
+export function Testimonial({ id }: { id: string }) {
   return (
-    <section className="mt-20 w-full" id={id}>
-      <h2 className="text-3xl font-extrabold lg:text-5xl text-center">Testimonials</h2>
-      <div className="container mt-10 m-auto">
-        <div className="flex flex-col items-center text-center">
-          {
-            contents.map((item, index) => (
-              <div key={`${item.author}-${index}`}>
-                <p className="mb-16 max-w-4xl px-8 font-medium lg:text-3xl mt-10">
-                  &ldquo;{item.quote}&rdquo;
-                </p>
-                <div className="flex items-center justify-center gap-2 md:gap-4">
-                  <Avatar className="size-12 md:size-16">
-                    <AvatarImage src={item.author.avatar.src} alt={item.author.avatar.alt} />
-                    <AvatarFallback>{item.author.name}</AvatarFallback>
-                  </Avatar>
-                  <div className="text-left">
-                    <p className="text-sm font-medium md:text-base">{item.author.name}</p>
-                    <p className="text-sm text-muted-foreground md:text-base">
-                      {item.author.role}
-                    </p>
-                  </div>
+    <>
+      <h2 className="text-3xl font-extrabold lg:text-5xl text-center mt-20 w-full">Testimonials</h2>    
+      <section className="mt-10 w-full" id={id}>
+        <Carousel
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+          orientation="vertical"
+          className="w-full"
+        >
+          <CarouselContent className="-mt-1 h-[290px]">
+            {contents.map((item, index) => (
+              <CarouselItem key={index} className="pt-1 md:basis-1/2">
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex items-center justify-center p-6">
+                      <div className="p-1 text-center">
+                        <p className="px-8 font-medium lg:text-3xl">
+                          &ldquo;{item.quote}&rdquo;
+                        </p>
+                        <div className="flex items-center justify-center gap-2 md:gap-1">
+                          <Avatar className="size-12 md:size-16">
+                            <AvatarImage src={item.author.avatar.src} alt={item.author.avatar.alt} />
+                            <AvatarFallback>{item.author.name}</AvatarFallback>
+                          </Avatar>
+                        </div>
+                      </div>
+                    </CardContent>
+                  </Card>
                 </div>
-              </div>
-            ))
-          }
-        </div>
-      </div>
-    </section>
-  );
-};
-
-export { Testimonial };
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </section>
+    </>
+  )
+}
