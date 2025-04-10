46214baddbe7181824764cece8aeb653fd381727
Zafar Saleem <zafarsaleem3@gmail.com>
2025-04-10 19:19:25 +0100

    Updates

--- _next/static/chunks/pages/_app-4fc04bc7eecc7e22.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{43317:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(1525)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(43317),n(26281))),_N_E=_.O()}]);
