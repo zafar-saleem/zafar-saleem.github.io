698b192779109059fe08eeca5c2cf63c17434514
Zafar Saleem <zafarsaleem3@gmail.com>
2025-04-12 21:43:38 +0100

    feat: minor updates in hero section

--- app/components/hero/contents.tsx
+++ app/components/hero/contents.tsx
@@ -5,7 +5,7 @@ export const contents = {
   descs: [
     "If your project involves online store, I CAN HELP, here is how:",
     "Don't have online store? I will build it for you.",
-    "You need help in Web Portal? I will help build Web Portal for you.",
+    "You need help in web portal? I will help build web portal for you.",
     "Don't have e-commerce website? I will built it for you."
   ],
   button: {
