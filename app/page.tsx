171a9b71ac4c4d6b01b314f84bbc0b45973d37d3
Zafar Saleem <zafarsaleem3@gmail.com>
2025-04-15 14:50:12 +0100

    feat: increase max width to main container

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { Cta } from "./components/call-to-action";
 
 export default function Home() {
   return (
-    <div className="m-auto max-w-7xl p-3 lg:p-0">
+    <div className="m-auto max-w-8xl p-3 lg:p-0">
       <Navbar />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
