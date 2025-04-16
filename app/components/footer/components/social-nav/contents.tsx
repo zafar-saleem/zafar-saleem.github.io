86d6bb84634e38becd00ea2697fb6a1c9c29a638
Zafar Saleem <zafarsaleem3@gmail.com>
2025-04-16 17:00:13 +0100

    feat: replace twitter label

--- app/components/footer/components/social-nav/contents.tsx
+++ app/components/footer/components/social-nav/contents.tsx
@@ -22,7 +22,7 @@ export const social = {
           alt: "Zafar Saleem LinkedIn.com profile",
         },
         { 
-          text: "X (Formerly Twitter)", 
+          text: "X/Twitter", 
           url: "https://x.com/zafartalks",
           logo: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiMxZjFmMWYiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS14Ij48cGF0aCBkPSJNMTggNiA2IDE4Ii8+PHBhdGggZD0ibTYgNiAxMiAxMiIvPjwvc3ZnPg==",
           alt: "Zafar Saleem X.com profile",
