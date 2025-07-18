158b0c59f1f4a5119b4e6126a36964bae1198b2c
Zafar Saleem <zafarsaleem3@gmail.com>
2025-07-18 14:12:51 +0100

    feat: added parallax to services section

--- app/components/services/services.tsx
+++ app/components/services/services.tsx
@@ -7,15 +7,16 @@ import {
 } from "@/components/ui/card";
 import { contents } from "./contents";
 import Image from "next/image";
+import styles from "./services.module.css";
 
 export const Services = ({ id }: { id: string }) => {
   return (
     <div className="w-full mt-15 text-center" id={id}>
       <h2 className="text-3xl font-extrabold lg:text-5xl text-center mt-10">Services</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
+      <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10`}>
         {
           contents.map((item, index) => (
-            <Card key={`${item.title}-${index}`}>
+            <Card key={`${item.title}-${index}`} className={`${styles.card}`}>
               <CardHeader>
                 <Image src={item.icon} width={75} height={75} alt="icon" className="m-auto mb-5" />
                 <CardTitle>{item.title}</CardTitle>
